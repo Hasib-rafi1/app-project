d639190415245e677eeaed30a48d326f385a8b22
gargi sharma <gargisharma5292@gmail.com>
2019-02-01 11:52:39 -0500

    Changes in main file and controller file

---
+++ SOEN_6441_RiskGame/src/controller/GameController.java
@@ -0,0 +1,33 @@
+package controller;
+
+/**
+ * 
+ * @author Gargi Sharma
+ * @version 1.0.0
+ */
+public class GameController {
+	
+	public static void generateMap() {
+		System.out.println("generate map ");
+	}
+
+
+	public static void editMap() {
+		// TODO Auto-generated method stub
+		System.out.println("case2 ");
+		
+	}
+	
+	public static void startGame() {
+		// TODO Auto-generated method stub
+		System.out.println("case3 ");		
+	}
+
+	public static void loadGame() {
+		// TODO Auto-generated method stub
+		System.out.println("case4 ");		
+	}
+
+	
+	
+}
--- SOEN_6441_RiskGame/src/main.java
+++ SOEN_6441_RiskGame/src/main.java
@@ -1,9 +1,81 @@
 
-public class main {
+/**
+ * @author Gargi Sharma
+ * @version 1.0.0
+ */
+import java.util.Scanner;
+import controller.GameController;
 
+
+/*
+ * main class to start Game
+ */
+public class main {
+	
+	/*
+	 * Main method
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test my main function");
+		
+		
+		GameController gameController = new GameController();
+
+		int selectOption = 0;
+		boolean ready_state = false;
+		do {
+			selectOption = displayMainMenu();
+			switch (selectOption) {			
+
+			case 1:
+				GameController.generateMap();
+				break;
+			case 2:
+				GameController.editMap();
+				break;
+			case 3:
+				GameController.startGame();
+
+				break;
+			case 4:
+				GameController.loadGame();
+
+				break;
+			case 5:
+				System.exit(0);
+			default :
+			
+				System.err.println("\n\t Error! Please select valid option from the above Options(1 to 5):");				
+				break;
+			}
+
+		} while (selectOption != 5);
+		System.exit(0);
+
 	}
+
+	/**
+	 * method for Displaying main menu for game 
+	 * @return 
+	 */
+	public static int displayMainMenu() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("\n==================================");
+		System.out.println("\t Risk Board Game");
+		System.out.println("==================================");
+		System.out.println("\n 1. Generate Map");
+		System.out.println("\n 2. Edit Map");
+		System.out.println("\n 3. Start Game");
+		System.out.println("\n 4. Load Game");
+		System.out.println("\n 5. Exit ");
+		System.out.println("\n Enter your Choice from the above Options(1 to 5): ");
+		return scanner.nextInt();
+	}
+
+
+
+
+
+
+
 
 }
