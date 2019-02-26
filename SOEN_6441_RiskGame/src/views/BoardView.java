a9d167b50cd347bb9cd300cd8fa6648671a7bc24
naren <naren@LAPTOP-F912ETFH.wireless.concordia.ca>
2019-02-25 20:02:45 -0500

    update

--- SOEN_6441_RiskGame/src/views/BoardView.java
+++ SOEN_6441_RiskGame/src/views/BoardView.java
@@ -1,21 +1,31 @@
 package views;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.TitledBorder;
 
-
+import helper.Colors;
 
 import model.CountryViewModel;
+import model.Game;
 
 /**
  * Risk game view designed in java swing
@@ -24,45 +34,48 @@ import model.CountryViewModel;
  *
  */
 public class BoardView implements Observer {
-	private static JFrame var_gameWindow = null;
-	private static JPanel var_gameAction;
+	private static JFrame frame_gameWindow = null;
+	private static JPanel panel_gameAction;
 
 	// Map variables
-	private static JLabel var_map;
-	private static JScrollPane var_mapScrollPane = null;
-	private static HashMap<String, Component> var_hashMap = new HashMap<>();
+	private static JLabel lab_map;
+	private static JScrollPane pane_mapScrollPane = null;
+	private static HashMap<String, Component> map_hashMap = new HashMap<>();
 
 	// Phase variables
-	private static JLabel var_gamePhase;
-	private static JLabel var_nameofPhase;
+	private static JLabel lab_gamePhase;
+	private static JLabel lab_nameofPhase;
 
 	// Initialization variables
-	private static JLabel var_initialisation;
-	private static JLabel var_playersTurn;
-	private static JLabel var_armiesLeft;
+	private static JLabel lab_initialisation;
+	private static JLabel lab_playersTurn;
+	private static JLabel lab_armiesLeft;
 
 	// Reinforcement variables
-	private static JLabel var_reinforcement;
-	private static JLabel var_unassignedReinforcement;
+	private static JLabel lab_reinforcement;
+	private static JLabel lab_unassignedReinforcement;
 
 	// Fortification variables
-	private static JLabel var_fortification;
-	private static JComboBox<String> var_sourceCountry;
+	private static JLabel lab_fortification;
+	private static JComboBox<String> combo_countrySource;
 
-	private static JComboBox<String> destinationCountry;
-	private static JComboBox<String> noOfArmyToMoveJcomboBox;
-	private static JButton fortificationMoveButton = new JButton("Move Army");
+	private static JComboBox<String> combo_countryDestination;
+	private static JComboBox<String> combo_armyToMove;
+	private static JButton button_moveFortification = new JButton("Move Army");
 
-	/*String activePlayerName = null;
+	String activePlayerName = null;
     int activePlayerId;
-    EnumColor activePlayerColor = null;
-	String activePlayerUnassignedArmiesCount, reinforcementUnassignedArmiesCount;   
+    Colors activePlayerColor = null;
+	String activePlayerUnassignedArmiesCount, reinforcementUnassignedArmiesCount;  
     String mapPath;
     ArrayList<CountryViewModel> countryList = new ArrayList<CountryViewModel>();
-    PhaseEnum phase;*/
+   // PhaseEnum phase;
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
+		Game game = ((Game)arg0);
+	     
+	  //   mapPath = game.getMap().+ "World.bmp";
 		
 	}
 
@@ -82,14 +95,28 @@ public void gameWindowLoad() {
 	//fortification();
 	//gamePhase();
 	actionPlan();
-	var_gameWindow.setSize(1250, 700);
-	var_gameWindow.setVisible(true);
-	var_gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame_gameWindow.setSize(1250, 700);
+	frame_gameWindow.setVisible(true);
+	frame_gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
 }
 public void actionPlan() {
-	var_gameWindow= new JFrame("Risk Game");
-	var_gameAction=new JPanel(null);
+	frame_gameWindow= new JFrame("Risk Game");
+	panel_gameAction=new JPanel(null);
+	File imageFile = null;
+	
+	imageFile = new File(mapPath);
+	Image image;
+	ImageIcon icon = null;
+	try {
+		image = ImageIO.read(imageFile);
+		icon = new ImageIcon(image);
+	} catch (IOException e) {
+
+		e.printStackTrace();
+	}
+
+	lab_map = new JLabel(icon);
 	
 }
 /**
@@ -108,7 +135,13 @@ public void reinforcements() {
  * Method for fortification implementation
  */
 public void fortification() {
-
+	lab_fortification = new JLabel();
+	lab_fortification.setBorder(
+			BorderFactory.createTitledBorder(null, "Fortification Phase", TitledBorder.DEFAULT_JUSTIFICATION,
+					TitledBorder.DEFAULT_POSITION, new Font("SansSerif", Font.PLAIN, 12), Color.BLUE));
+	lab_fortification.setBounds(lab_reinforcement.getX(),
+			lab_reinforcement.getY() + 10 + lab_reinforcement.getHeight(), lab_reinforcement.getWidth(),
+			140);
 }
 /**
  * Method that updates the phase of the game
