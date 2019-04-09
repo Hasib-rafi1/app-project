9bc2ea2c6efa2cb5a52ed8eb2cd31069295cb240
Gargi Sharma <gargisharma92@gmail.com>
2019-04-09 10:17:00 -0400

    Delete build1 backup folder

--- SOEN_6441_RiskGame/Build1Backup/src/helper/GamePhase.java
+++
@@ -1,9 +0,0 @@
-package helper;
-/**
- * This enum is containging the phases name of the game
- * @author Hasibul Huq
- *
- */
-public enum GamePhase {
-	Startup, Reinforcement, Attack, Fortification
-}
